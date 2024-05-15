0af1c484055693cc3223da48bc6e2a886cd6409c
YoshitakaKyougoku <114034035+YoshitakaKyougoku@users.noreply.github.com>
2024-05-15 11:31:59 +0900

    react-router v6にupdate

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client"; // 更新されたインポート
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { CookiesProvider } from "react-cookie";
 import { Provider } from "react-redux";
-import { store } from "./store"
+import { store } from "./store";
 
-ReactDOM.render(
+// root要素を取得
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error('Failed to find the root element');
+
+// React 18の新しいAPIを使用してrootを作成
+const root = createRoot(rootElement);
+
+// root.renderを使用してAppコンポーネントをレンダリング
+root.render(
   <Provider store={store}>
     <CookiesProvider>
       <App />
     </CookiesProvider>
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
